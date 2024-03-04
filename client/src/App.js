4c542ca4a7f910fd3525bc453b2f636525aaec8a
crampussm <swaparup36@gmail.com>
2024-03-05 00:34:01 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Landing from './components/Landing';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='login/' element={<Login/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
