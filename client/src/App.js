e71850ea15a8e1f6d9c52bbf2ae7ffcc9bbb9e68
crampussm <swaparup36@gmail.com>
2024-03-07 03:33:16 +0530

    test

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className='w-full'>
+    <div className='w-full hi'>
       <Routes>
         <Route path='/' element={<Landing/>}/>
         <Route path='login/' element={<Login/>}/>
