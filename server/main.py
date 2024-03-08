c768e57a22a487b141430484f7f40e7bfec6ec80
Aman Mishra <151461149+am12an34@users.noreply.github.com>
2024-03-08 15:23:28 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,12 +1,11 @@
 from flask import *
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 import pyrebase
 import jwt
 from datetime import timedelta,datetime
 from functools import wraps
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app,origins="http://localhost:3000")
 
 from Apps import*
 import configuration
